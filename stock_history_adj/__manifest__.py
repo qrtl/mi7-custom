d0413620bb21b076250bd0d98828144e3140d83b
Yoshi Tashiro <tashiro@quartile.co>
2020-07-18 15:50:58 +0800

    [ADD] stock_history_adj

---
+++ stock_history_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock History Adj",
+    "category": "Stock",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "10.0.1.0.0",
+    "depends": ["stock_account"],
+    "installable": True,
+}
---
+++ stock_history_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+* Overrides the read_group method of StockHistory to bypass the misterious product access error which has been described in task 1076.
