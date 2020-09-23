d7161d245011e766fbbbeda9f8568f423a0457a8
tslai <tl@quartile.co>
2020-09-23 14:26:40 +0800

    [ADD] website_auth_view_mi7

---
+++ website_auth_view_mi7/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Login/Signup Page Adjustments",
+    "category": "Website",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "10.0.1.0.0",
+    "depends": [
+        "auth_signup",
+    ],
+    "data": ["views/templates.xml"],
+    "installable": True,
+}
---
+++ website_auth_view_mi7/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Overwrite template adjustments from Clarico module, customize the signup, login and password reset pages.
