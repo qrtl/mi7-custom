d9c9eb14adff30994327e7ec6bb34a4c5dafdd80
tslai <tl@quartile.co>
2020-10-06 18:57:18 +0800

    [IMP] Move auth page texts to website config settings

---
+++ website_auth_view_mi7/models/website_config_settings.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields
+
+
+class WebsiteConfigSettings(models.TransientModel):
+    _inherit = 'website.config.settings'
+
+    login_page_header_text = fields.Html(
+        "Login Page Header Text",
+        related='website_id.login_page_header_text'
+    )
+    login_page_signup_text = fields.Html(
+        "Login Page User Register Text",
+        related='website_id.login_page_signup_text'
+    )
+    signup_page_header_text = fields.Html(
+        "Signup Page Header Text",
+        related='website_id.signup_page_header_text'
+    )
+    signup_page_terms_text = fields.Html(
+        "Signup Page Terms and Conditions Text",
+        related='website_id.signup_page_terms_text'
+    )
+    password_reset_page_header_text = fields.Html(
+        "Password Reset Page Header Text",
+        related='website_id.password_reset_page_header_text'
+    )
