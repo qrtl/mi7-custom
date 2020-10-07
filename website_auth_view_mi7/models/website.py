30969c8cae156a7ee87273ee1e98df83154e8105
tslai <tl@quartile.co>
2020-10-07 13:46:44 +0800

    [IMP] Add sanitize attribute, adjust translations

--- website_auth_view_mi7/models/website.py
+++ website_auth_view_mi7/models/website.py
@@ -7,14 +7,14 @@ from odoo import fields, models
 class Website(models.Model):
     _inherit = "website"
 
-    login_page_header_text = fields.Html("Login Page Header Text", translate=True,)
+    login_page_header_text = fields.Html("Login Page Header Text", translate=True, sanitize=False)
     login_page_signup_text = fields.Html(
-        "Login Page User Register Text", translate=True,
+        "Login Page User Register Text", translate=True, sanitize=False
     )
-    signup_page_header_text = fields.Html("Signup Page Header Text", translate=True,)
+    signup_page_header_text = fields.Html("Signup Page Header Text", translate=True, sanitize=False)
     signup_page_terms_text = fields.Html(
-        "Signup Page Terms and Conditions Text", translate=True,
+        "Signup Page Terms and Conditions Text", translate=True, sanitize=False
     )
     password_reset_page_header_text = fields.Html(
-        "Password Reset Page Header Text", translate=True,
+        "Password Reset Page Header Text", translate=True, sanitize=False
     )
