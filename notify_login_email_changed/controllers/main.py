0b3df84c5cddf0b7a7e43a49d9b5ba9c0f52d4da
Le Hoang An <anlh@trobz.com>
2021-03-15 14:38:58 +0700

    [UP] Move code from write function of partner to controller /my/account

---
+++ notify_login_email_changed/controllers/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_portal.controllers.main import website_account
+
+
+class WebsiteAccount(website_account):
+
+    @http.route(['/my/account'], type='http', auth='user', website=True)
+    def details(self, redirect=None, **post):
+        partner = request.env.user.partner_id
+        old_email = partner.email
+        response = super(WebsiteAccount, self).details(
+            redirect=redirect, **post)
+        if post and post['email'] and old_email != post['email']:
+            partner._send_email(old_email, post['email'])
+
+        return response
--- notify_login_email_changed/models/res_partner.py
+++ notify_login_email_changed/models/res_partner.py
@@ -8,8 +8,12 @@ from odoo import models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    def _send_email(self, mail_template, old_email="", new_email=""):
+    def _send_email(self, old_email="", new_email=""):
         self.ensure_one()
+        module = "notify_login_email_changed"
+        mail_ex_id = "email_template_notify_email_change"
+        mail_template = self.env.ref("{}.{}".format(module, mail_ex_id))
+
         ctx = {
             "default_composition_mode": "comment",
             "mail_create_nosubscribe": True,
@@ -21,14 +25,3 @@ class ResPartner(models.Model):
         template = mail_template.with_context(ctx)
         template.send_mail(self.id, force_send=True)
         return True
-
-    def write(self, vals):
-        if vals.get("email"):
-            module = "notify_login_email_changed"
-            mail_ex_id = "email_template_notify_email_change"
-            mail_template = self.env.ref("{}.{}".format(module, mail_ex_id))
-            new_email = vals["email"]
-            for partner in self:
-                partner._send_email(mail_template, partner.email, new_email)
-
-        return super(ResPartner, self).write(vals)
