bdfe42528ee35cecc84eda94e21de3bd4390bc2f
Yoshi Tashiro <tashiro@quartile.co>
2021-03-20 16:56:10 +0800

    readme, translation

--- sale_automatic_workflow_propose/models/sale_order.py
+++ sale_automatic_workflow_propose/models/sale_order.py
@@ -2,14 +2,16 @@
 # Copyright 2021 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import api, models
+
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-
     def _update_vals(self, vals):
-        process = self.env["sale.workflow.process"].search([("apply_to", "=", vals["order_type"])])[:1]
+        process = self.env["sale.workflow.process"].search(
+            [("apply_to", "=", vals["order_type"])]
+        )[:1]
         if process:
             vals["workflow_process_id"] = process.id
         return vals
--- sale_automatic_workflow_propose/models/sale_workflow_process.py
+++ sale_automatic_workflow_propose/models/sale_workflow_process.py
@@ -2,7 +2,7 @@
 # Copyright 2021 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleWorkflowProcess(models.Model):
