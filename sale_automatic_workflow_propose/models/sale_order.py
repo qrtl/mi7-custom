7ecb6d13efe04c94ca6658f9b484260394c08e7f
Yoshi Tashiro <tashiro@quartile.co>
2021-03-21 13:27:14 +0800

    move the condition inside _update_vals()

--- sale_automatic_workflow_propose/models/sale_order.py
+++ sale_automatic_workflow_propose/models/sale_order.py
@@ -9,21 +9,20 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _update_vals(self, vals):
-        process = self.env["sale.workflow.process"].search(
-            [("apply_to", "=", vals["order_type"])]
-        )[:1]
-        if process:
-            vals["workflow_process_id"] = process.id
+        if vals.get("order_type"):
+            process = self.env["sale.workflow.process"].search(
+                [("apply_to", "=", vals["order_type"])]
+            )[:1]
+            if process:
+                vals["workflow_process_id"] = process.id
         return vals
 
     @api.model
     def create(self, vals):
-        if vals.get("order_type"):
-            vals = self._update_vals(vals)
+        vals = self._update_vals(vals)
         return super(SaleOrder, self).create(vals)
 
     @api.multi
     def write(self, vals):
-        if vals.get("order_type"):
-            vals = self._update_vals(vals)
+        vals = self._update_vals(vals)
         return super(SaleOrder, self).write(vals)
