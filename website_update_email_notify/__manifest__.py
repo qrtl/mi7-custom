b90ada67b896c136d898a9a30c022da72d40d881
tslai <tl@quartile.co>
2021-03-22 16:23:48 +0800

    [IMP] Adjust readme, add translations

--- website_update_email_notify/__manifest__.py
+++ website_update_email_notify/__manifest__.py
@@ -2,13 +2,13 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Notify email when login email change",
+    "name": "Portal Login Change",
     "category": "Website",
     "license": "LGPL-3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "version": "10.0.1.0.0",
-    "depends": ["mail", "website_portal"],
+    "depends": ["mail", "pro_mi7_website_sale_ec"],
     "data": ["views/templates.xml", "views/email_templates.xml"],
     "installable": True,
 }
