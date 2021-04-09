a116f8e3592296e7a179c45e42782c4d7601867c
Yoshi Tashiro <tashiro@quartile.co>
2021-04-09 18:00:30 +0800

    [IMP] account_invoice_validate_send_email: not_send_invoice option in payment terms

---
+++ account_invoice_validate_send_email/models/account_payment_term.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountPaymentTerm(models.Model):
+    _inherit = "account.payment.term"
+
+    not_send_invoice = fields.Boolean(
+        string="Not Auto-send Invoice",
+        help="When this field is selected, no email will be automatically sent to the customer despite the settings of the linked workflow process.",
+    )
