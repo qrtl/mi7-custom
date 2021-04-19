740e6810fa2b9bf3b49972d5a4cd5c5f07932bee
Yoshi Tashiro <tashiro@quartile.co>
2021-04-19 23:05:23 +0800

    update logic, add fields

--- stock_picking_yamato_csv/__manifest__.py
+++ stock_picking_yamato_csv/__manifest__.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 # Copyright 2021 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Stock Picking Export Report",
+    "name": "Stock Picking Yamato CSV",
     "version": "10.0.1.0.0",
     "category": "Stock",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "depends": ["sale_stock", "pro_mi7_website_sale_ec", "report_csv"],
     "data": [
-        "security/ir.model.access.csv",
-        "data/stock_picking_export_report_data.xml",
-        "report/stock_picking_export_report.xml",
+        # "security/ir.model.access.csv",
+        # "data/stock_picking_export_report_data.xml",
+        # "data/stock_picking_yamato_csv_report.xml",
         "views/res_company_views.xml",
         "views/shipping_timerange_views.xml",
-        "views/stock_picking_export_report_views.xml",
+        # "views/stock_picking_export_report_views.xml",
         "views/stock_picking_views.xml",
     ],
 }
--- stock_picking_yamato_csv/report/__init__.py
+++ stock_picking_yamato_csv/report/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import report_stock_picking_export_csv
+from . import report_stock_picking_yamato_csv
 from . import stock_picking_export_report
