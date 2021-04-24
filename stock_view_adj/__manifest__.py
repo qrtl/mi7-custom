cbdccf0d52e7fd44024421e862a39dc7bb04db39
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2021-04-24 22:59:48 +0900

    st1 [1658][ADD] stock_view_adj (#30)

---
+++ stock_view_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock View Adjust",
+    "category": "Stock",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "10.0.1.0.0",
+    "depends": ["sale_stock"],
+    "data": ["views/stock_picking_views.xml"],
+    "installable": True,
+}
