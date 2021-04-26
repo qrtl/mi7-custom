8504818c21e494ca1af6a32358bd97106fab87ff
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2021-04-26 09:53:03 +0900

    st1 [1658][ADD] stock_picking_yamato_csv (#29)

---
+++ stock_picking_yamato_csv/models/shipping_timerange_ec.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ShippingTimerangeEc(models.Model):
+    _inherit = "shipping.timerange.ec"
+
+    delivery_time_categ = fields.Char(
+        "Delivery Time Category",
+        required=True,
+        help="This field is used for shipping instruction data export.",
+    )
