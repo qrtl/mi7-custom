696d0a4ca4e4f9b4548c95c1856c89d7e6d1d425
Yoshi Tashiro <tashiro@quartile.co>
2021-05-18 22:34:31 +0800

    [ADD] partner_attribute_mi7

---
+++ partner_attribute_mi7/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Partner Attribute",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["base"],
+    "data": ["views/res_partner_views.xml"],
+}
