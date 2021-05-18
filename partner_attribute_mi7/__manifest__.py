bc3fbb30d87cfbae048f792c8681af7648557d61
Yoshi Tashiro <tashiro@quartile.co>
2021-05-18 22:39:38 +0800

    adjust module name, translation

--- partner_attribute_mi7/__manifest__.py
+++ partner_attribute_mi7/__manifest__.py
@@ -2,7 +2,7 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Partner Attribute",
+    "name": "Partner Attributes",
     "version": "10.0.1.0.0",
     "category": "Sales",
     "website": "https://www.quartile.co/",
