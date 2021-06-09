bab2bc1ff3aa0204cdd2ca480d0bbb7a047ae90b
Yoshi Tashiro <tashiro@quartile.co>
2021-06-09 16:31:00 +0800

    update comment

--- stock_picking_yamato_csv/report/report_stock_picking_yamato_csv.py
+++ stock_picking_yamato_csv/report/report_stock_picking_yamato_csv.py
@@ -234,7 +234,8 @@ class StockPickingYamatoCSV(models.AbstractModel):
                 sale_line.price_reduce_taxinc - sale_line.price_reduce_taxexcl
             )
         for sale_line in picking.sale_id.order_line:
-            # For cash-on-delivery and delivery fees
+            # For cash-on-delivery and delivery fees. We assume that there will
+            # be no multiple deliveries for an order involving COD.
             if sale_line.product_id.default_code == "COD" or sale_line.is_delivery:
                 amt_taxinc += sale_line.product_uom_qty * sale_line.price_reduce_taxinc
                 amt_tax += sale_line.product_uom_qty * (
