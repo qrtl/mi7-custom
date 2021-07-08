dfa404911d9876004604c0fc14a9f445ae2292b5
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-07-08 15:25:40 +0800

    Edit UserError sentence

--- stock_picking_yamato_csv/report/report_stock_picking_yamato_csv.py
+++ stock_picking_yamato_csv/report/report_stock_picking_yamato_csv.py
@@ -265,8 +265,8 @@ class StockPickingYamatoCSV(models.AbstractModel):
             )
             if scheduled_date < today_date:
                 raise UserError(
-                    _("There is a delivery with a past scheduled date:\n%s")
-                    % ("\n".join(picking.mapped("name")))
+                    _("There is a delivery with a past scheduled date: %s")
+                    %(picking.name)
                 )
             # 伝票区分 '00' means that 送り状 will not be issued.
             slip_categ = "00"
