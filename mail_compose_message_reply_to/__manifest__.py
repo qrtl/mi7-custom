d4c18a0ff66564f8499fbbe44934ea8e0bd68659
tack-yabeQuartile <85266439+tack-yabeQuartile@users.noreply.github.com>
2021-07-16 15:20:38 +0900

    st1 [1691][ADD] mail_compose_message_reply_to (#65)
    * first commit. add mail_compose_message.py
    
    * change logic to limit scope_fill-only-when-no-reply-to
    
    * move to new module mail_compose_message_reply_to
    
    * tidy up files
    
    * tidy up files again and check pre-commit
    
    * clear unit_test
    
    * add description and change categories
    
    * generate README

---
+++ mail_compose_message_reply_to/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Compose Message Reply To",
+    "category": "Discuss",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "10.0.1.0.0",
+    "installable": True,
+}
