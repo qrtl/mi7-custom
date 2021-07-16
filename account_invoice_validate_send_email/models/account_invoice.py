feff9602c393c6c9d83db810875bb19752d8f480
TomohiroAbe-Quartile <84064473+TomohiroAbe-Quartile@users.noreply.github.com>
2021-07-16 14:53:32 +0900

    st1 [1691][IMP] action_send-include-red_list (#64)
    Include red_list in action_send and not_send_invoice.

--- account_invoice_validate_send_email/models/account_invoice.py
+++ account_invoice_validate_send_email/models/account_invoice.py
@@ -66,11 +66,17 @@ class AccountInvoice(models.Model):
         res = super(AccountInvoice, self).action_invoice_open()
         base_url = self.env["ir.config_parameter"].get_param("web.base.url")
         for invoice in self:
+            if invoice.type not in ("out_invoice", "out_refund"):
+                continue
             term = invoice.payment_term_id
+            orders = invoice.invoice_line_ids.mapped("sale_line_ids").mapped("order_id")
+            pickings = self.env["stock.picking"].search([("sale_id", "in", orders.ids)])
+
             if (
                 invoice.send_invoice
                 and not (term and term.not_send_invoice)
                 and not invoice.picking_ids.filtered(lambda x: x.not_send_invoice)
+                and not (pickings and pickings.filtered(lambda x: x.not_send_invoice))
                 and not invoice.invoice_sent
             ):
                 # TODO We may want to adjust/remove web_url - the value points
