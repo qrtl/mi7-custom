d120bd145a3337a913c52b1388d010df49269e98
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-08-17 10:10:36 +0800

    Add compute logic(still not worked)

--- stock_delivery_result_import/models/stock_picking.py
+++ stock_delivery_result_import/models/stock_picking.py
@@ -2,7 +2,7 @@
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 from odoo.addons.queue_job.job import job
 
@@ -11,10 +11,10 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     log_id = fields.Many2one("data.import.log", string="Log")
-    yamato_slip_number = fields.Char(
+    yamato_slip_number = fields.Text(
         "Yamato Slip Number",
         help="Delivery Slip Number of Yamato Transport CO.,.",
-        store=True,
+        compute="_create_slip_number",
     )
 
     @job()
@@ -28,3 +28,11 @@ class StockPicking(models.Model):
         self.do_transfer()
         if not self.log_id.picking_ids.filtered(lambda x: x.state != "done"):
             self.log_id.sudo().write({"state": "done"})
+
+    @api.depends('picking_ids')
+    def _create_slip_number(self):
+        tracking_numbers = []
+        for picking in self.picking_ids:
+            if picking.carrier_tracking_ref:
+                tracking_numbers += [picking.carrier_tracking_ref + ",\n"]
+        return tracking_numbers
