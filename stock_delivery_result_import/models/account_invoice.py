f2bd2b394fa5fad92ef302fc84f9e593d5c2a40e
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-08-17 11:12:05 +0800

    edit logic

--- stock_delivery_result_import/models/account_invoice.py
+++ stock_delivery_result_import/models/account_invoice.py
@@ -4,19 +4,21 @@
 
 from odoo import api, fields, models
 
+
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     yamato_slip_number = fields.Text(
         "Yamato Slip Number",
         help="Delivery Slip Number of Yamato Transport CO.,.",
-        compute="_compute_slip_number",
+        compute="_compute_yamato_slip_number",
     )
 
     @api.depends("picking_ids")
-    def _compute_slip_number(self):
-        tracking_numbers = []
-        for picking in self.picking_ids:
-            if picking.carrier_tracking_ref:
-                tracking_numbers += [picking.carrier_tracking_ref + ",\n"]
-        return tracking_numbers
+    def _compute_yamato_slip_number(self):
+        for invoice in self:
+            tracking_numbers = []
+            for picking in invoice.picking_ids:
+                if picking.carrier_tracking_ref:
+                    tracking_numbers.append(picking.carrier_tracking_ref + ",\n")
+            invoice.yamato_slip_number = tracking_numbers
