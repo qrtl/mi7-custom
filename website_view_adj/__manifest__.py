bc361bc3b2e549657c77c4cff4b98084a373876b
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-08-30 15:47:06 +0800

    initial commit

---
+++ website_view_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website View Adjust",
+    "category": "Web",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "10.0.1.0.0",
+    "depends": ["website"],
+    "data": ["views/website_snipet_views.xml"],
+    "installable": True,
+}
