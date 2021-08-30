315ecade15b202b62c6477450fe866297b694f9f
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-08-30 10:16:01 +0800

    Rename field and edit compute method

--- account_invoice_validate_send_email/models/account_invoice.py
+++ account_invoice_validate_send_email/models/account_invoice.py
@@ -18,8 +18,10 @@ class AccountInvoice(models.Model):
     )
     web_url = fields.Char()
 
-    slip_number = fields.Text(
-        "Slip Number", help="Delivery Slip Number", compute="_compute_slip_number",
+    carrier_tracking_refs = fields.Char(
+        "Tracking References",
+        help="Delivery slip numbers taken from the linked deliveries.",
+        compute="_compute_carrier_tracking_refs",
     )
 
     def _get_mail_template(self):
@@ -114,13 +116,12 @@ class AccountInvoice(models.Model):
                 self.invoice_sent = True
         return True
 
-    # This method not supposed red_order. Because red_order haven't delivery of own company.
-    @api.multi
-    def _compute_slip_number(self):
-        if self.type not in ("out_refund"):
-            for invoice in self:
-                tracking_numbers = ""
-                for picking in invoice.picking_ids:
-                    if picking.carrier_tracking_ref:
-                        tracking_numbers += picking.carrier_tracking_ref + "\n"
-                invoice.slip_number = tracking_numbers
+    # Return cases, where there is no direct link from the invoice to the pickings,
+    # are outside of the scope of showing the tracking references to the customer.    @api.multi
+    def _compute_carrier_tracking_refs(self):
+        for invoice in self:
+            invoice.carrier_tracking_refs = ", ".join(
+                x.carrier_tracking_ref
+                for x in invoice.picking_ids
+                if x.carrier_tracking_ref
+            )
