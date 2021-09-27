47bb0f1cc03d63a79d1c05a3adef62f90be57309
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-09-27 14:52:09 +0800

    edit actions

--- stock_carrier_info/__manifest__.py
+++ stock_carrier_info/__manifest__.py
@@ -9,7 +9,7 @@
     "license": "AGPL-3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co/",
-    "depends": ["sale_stock"],
+    "depends": ['queue_job', "sale_stock"],
     "data": [
         "security/carrier_info_group.xml",
         "security/ir.model.access.csv",
