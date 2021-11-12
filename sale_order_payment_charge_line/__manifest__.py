1e065d4d8d0a701a72017bbf0febae1e55abf6a7
TomohiroAbe-Quartile <84064473+TomohiroAbe-Quartile@users.noreply.github.com>
2021-11-12 12:28:21 +0900

    st1 [2088][ADD] sale_order_payment_charge_line (#94)
    Add module: sale_order_payment_charge_line

---
+++ sale_order_payment_charge_line/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale Order Payment Charge Line",
+    "version": "10.0.1.0.0",
+    "category": "Sale",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["sale"],
+    "data": ["views/product_template_views.xml"],
+    "installable": True,
+}
