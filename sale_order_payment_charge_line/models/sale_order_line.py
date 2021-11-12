e09bb864661e400c229a42958d9b4a02b9b6f74f
TomohiroAbe-Quartile <84064473+TomohiroAbe-Quartile@users.noreply.github.com>
2021-11-12 12:28:21 +0900

    st1 [2088][ADD] sale_order_payment_charge_line (#94)
    Add module: sale_order_payment_charge_line

---
+++ sale_order_payment_charge_line/models/sale_order_line.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    is_payment_charge = fields.Boolean(
+        "Payment Charge", related="product_id.is_payment_charge", readonly=True,
+    )
