aeb4cdee660427e1544eba4fee463fe7c7bde018
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2021-11-17 17:43:12 +0900

    [2110][ADD] auth_block_signup (#95)

---
+++ auth_block_signup/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Block Signup",
+    "version": "10.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["base"],
+}
