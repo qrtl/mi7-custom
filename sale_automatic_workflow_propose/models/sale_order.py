a665c67ca036fe6d7c727d0e64a088bf3aa54585
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-01-24 17:55:52 +0900

    [2253][IMP] sale_automatic_workflow_propose, stock_picking_yamato_csv (#108)
    * Change order_type field to user_type

--- sale_automatic_workflow_propose/models/sale_order.py
+++ sale_automatic_workflow_propose/models/sale_order.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2021 Quartile Limited
+# Copyright 2021-2022 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
@@ -9,9 +9,9 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _update_vals(self, vals):
-        if vals.get("order_type"):
+        if vals.get("user_type"):
             process = self.env["sale.workflow.process"].search(
-                [("apply_to", "=", vals["order_type"])]
+                [("apply_to", "=", vals["user_type"])]
             )[:1]
             if process:
                 vals["workflow_process_id"] = process.id
