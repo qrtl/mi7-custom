620a366289f4526d8ad6670768c8c159efa5e62a
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-01-24 17:55:52 +0900

    st1 [2253][IMP] sale_automatic_workflow_propose, stock_picking_yamato_csv (#108)
    * switch from order_type to customer_class
    
    * change from customer_class to user_type

--- sale_automatic_workflow_propose/__manifest__.py
+++ sale_automatic_workflow_propose/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2021 Quartile Limited
+# Copyright 2021-2022 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Automatic Workflow Propose",
@@ -7,8 +7,8 @@
     "license": "AGPL-3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
-    "version": "10.0.1.0.0",
-    "depends": ["sale_automatic_workflow", "pro_mi7_website_sale_ec"],
+    "version": "10.0.1.1.0",
+    "depends": ["sale_automatic_workflow", "sale_user_type"],
     "data": ["views/sale_workflow_process_views.xml"],
     "installable": True,
 }
