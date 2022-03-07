90416f0fdae315851f13599f9952e57ed711db9c
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-03-07 16:55:18 +0900

    [2253][IMP] pro_mi7_website_sale_ec: break the module into smaller ones with specific purposes (#165)
    Added:
    - auth_no_reset_password
    - mail_compose_message_type_email
    - mail_message_skip_notify_by_email
    - partner_occupation
    - product_form_alternative_product_tab
    - product_invoice_policy_constraint
    - sale_physical_product
    - sale_transaction_type
    - sale_user_type
    - stock_product_availability
    - website_portal_product
    - website_register_terms
    - website_sale_cash_on_delivery
    - website_sale_delivery_date
    - website_sale_delivery_physical_product
    - website_sale_pricelist_by_promo
    - website_sale_product_availability
    
    Updated (those including model/data updates):
    - pro_mi7_license
    - pro_mi7_product
    - pro_mi7_sale
    - pro_mi7_website_sale_ec
    - pro_mi7_xchange
    - sale_user_type
    - stock_product_availability

--- sale_user_type/__manifest__.py
+++ sale_user_type/__manifest__.py
@@ -5,11 +5,15 @@
 {
     "name": "Sale User Type",
     "version": "10.0.1.0.0",
-    "category": "Sale",
+    "category": "Sales",
     "license": "LGPL-3",
     "author": "MI Seven Japan, Quartile Limited",
     "website": "https://www.quartile.co",
     "depends": ["sale_stock"],
-    "data": ["data/ir_sequence_data.xml", "views/sale_order_views.xml",],
+    "data": [
+        "data/ir_sequence_data.xml",
+        "views/res_partner_views.xml",
+        "views/sale_order_views.xml",
+    ],
     "installable": True,
 }
