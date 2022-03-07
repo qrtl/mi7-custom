566d0f29489708aa74dcbd97d4473ce87f63a8dd
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-03-07 17:38:16 +0900

    [2253][IMP] partner_attribute_mi7, stock_picking_yamato_csv, website_auth_view_mi7, website_update_email_notify (#109)
    Updated:
    - partner_attribute_mi7 (upgrade needed)
    - stock_picking_yamato_csv (upgrade needed)
    - website_auth_view_mi7 (upgrade needed)
    - website_update_email_notify (restart should suffice)
      - Remove pro_mi7_website_sale_ec dependency

--- website_update_email_notify/__manifest__.py
+++ website_update_email_notify/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2021 Quartile Limited
+# Copyright 2021-2022 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Portal Login Change",
@@ -8,10 +8,7 @@
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "version": "10.0.1.0.0",
-    # Need to have "pro_mi7_website_sale_ec" as a dependency,
-    # since this module overrides the detail() controller:
-    # https://github.com/qrtl/pro-mi7/blob/2a6f015f8928fb5748052ac819dbc04dff0ae4b5/pro_mi7_website_sale_ec/controllers/website_portal_main.py#L71-L107 # noqa
-    "depends": ["mail", "pro_mi7_website_sale_ec"],
+    "depends": ["mail", "website_portal"],
     "data": ["views/templates.xml", "views/email_templates.xml"],
     "installable": True,
 }
