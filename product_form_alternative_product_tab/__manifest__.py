6ecb4d150fed0d1ab7854636f7b1b9a5a8965476
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-03-07 16:55:18 +0900

    [2253][IMP] pro_mi7_website_sale_ec: break the module into smaller ones with specific purposes (#165)
    Added:
    - auth_no_reset_password
    - mail_compose_message_type_email
    - mail_message_skip_notify_by_email
    - partner_occupation
    - product_form_alternative_product_tab
    - product_invoice_policy_constraint
    - sale_physical_product
    - sale_transaction_type
    - sale_user_type
    - stock_product_availability
    - website_portal_product
    - website_register_terms
    - website_sale_cash_on_delivery
    - website_sale_delivery_date
    - website_sale_delivery_physical_product
    - website_sale_pricelist_by_promo
    - website_sale_product_availability
    
    Updated (those including model/data updates):
    - pro_mi7_license
    - pro_mi7_product
    - pro_mi7_sale
    - pro_mi7_website_sale_ec
    - pro_mi7_xchange
    - sale_user_type
    - stock_product_availability

---
+++ product_form_alternative_product_tab/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2021 MI Seven Japan
+# Copyright 2022 Quartile Limited
+{
+    "name": "Product Form Alternative Product Tab",
+    "category": "Product",
+    "version": "10.0.1.0.0",
+    "author": "MI Seven Japan, Quartile Limited",
+    "license": "Other proprietary",
+    "depends": ["pro_mi7_product", "website_sale"],
+    "data": ["views/product_template_views.xml"],
+    "installable": True,
+}
---
+++ product_form_alternative_product_tab/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Adds a tab in the product form to show alternative products.
