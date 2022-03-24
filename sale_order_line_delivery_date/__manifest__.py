25313364d24196f98c79d01d038054be22841c5c
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-03-24 14:29:38 +0900

    [2335][IMP] pro_mi7_ec_calendar, website_sale_delivery_date (#187)
    * Add sale_order_line_delivery_date module
    * Add website_frontend_stylesheet_mi7
    * Remove logic around sale.order.line delivery_date
    * Remove unused parts
    * Fix the logic around deliverable date range which was implemented based on misunderstandings
    * Improve usability around holiday calendar

---
+++ sale_order_line_delivery_date/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2021 MI Seven Japan
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Sale Order Line Delivery Date",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "author": "MI Seven Japan, Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["sale_stock"],
+    "data": ["views/sale_order_views.xml",],
+    "installable": True,
+}
---
+++ sale_order_line_delivery_date/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+This module does the following:
+
+- Adds Delivery Date field to sale.order.line, which is used just to record the expected
+  date of delivery for internal use (no logic added around this field).
+
+This module is not expected to be migrated to the newer version.
