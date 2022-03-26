7d8b6d10a92f75cbc7e4e0861e8f3c48f3c07dbc
Yoshi Tashiro <tashiro@quartile.co>
2022-03-26 13:38:04 +0800

    switch field from musicecosystems_name to alt_name

--- stock_picking_yamato_csv/__manifest__.py
+++ stock_picking_yamato_csv/__manifest__.py
@@ -3,19 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Picking Yamato CSV",
-    "version": "10.0.1.1.2",
+    "version": "10.0.1.1.3",
     "category": "Stock",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "sale_stock",
-        "pro_mi7_website_sale_product_list",
-        "pro_mi7_sale",
-        "website_sale_delivery_date",  # For delivery_date, delivery_time_id
+        "company_alternative_attribute",  # alt_name (res.company)
+        "pro_mi7_website_sale_product_list",  # TODO: Check why it's needed
         "report_csv",
+        "sale_stock",
         "sale_user_type",  # For user_type
+        "stock_product_availability",  # person (sale.order)
+        "website_sale_delivery_date",  # For delivery_date, delivery_time_id
     ],
     "data": [
         "views/res_partner_views.xml",
