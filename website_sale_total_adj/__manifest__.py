dca998968ed0be277da8c1ec61ac6c0e51940e04
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-04-09 21:36:53 +0900

    [2369] misc updates (#189)
    * add faq line, remove excessive line breaks
    * remove sale.order.line line_number
    * add company_alternative_attribute
    * remove shipment report
    * remove sale.order type field
    * add partner_postal_address
    * misc fixes, translation
    * normalize check_zip
    * consolidate address check/proposal logic
    * remove note field
    * add company_chop
    * add partner_attribute
    * remove sales order discount
    * remove csv.report.output
    * make version upgrade buttons visible in Safari 15.4
    * move threshold_value to stock_product_availability module
    * remove related.product.list
    * add product_intercompany_link
    * make company_id a related field
    * add product_maker
    * remove styling from the a tag
    * add purchase_create_sale_order
    * remove unused parts
    * remove pro_mi7_product
    * remove pro_website_sale_product_list
    * remove partner_ids of res.company
    * add sale_customer_order_info
    * update dependencies
    * remove product comment field
    * update get_min_date()
    * simplify mi7_cart_total template
    * fix product registration button
    * add website_sale_total_adj
    * remove group_id from website.menu
    * add website_sale_cart_check
    * add website_sale_cart_check_dl_qty
    * add product_pricelist_item_note, product_pricelist_item_view
    * remove pro_mi7_import
    * remove pro_mi7

---
+++ website_sale_total_adj/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2021 MI Seven Japan
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Website Sale Total Adj",
+    "category": "Website",
+    "version": "10.0.1.0.0",
+    "author": "MI Seven Japan, Quartile Limited",
+    "license": "LGPL-3",
+    "depends": ["website_sale"],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+}
---
+++ website_sale_total_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Adjusts the cart total presentation depending on the tax exclusive/inclusive setting.
