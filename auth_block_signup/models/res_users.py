d12c35fc3b7b843deb0abeee72d842e74fec4f45
gamhpang <gamhpang@1812@gmail.com>
2022-04-12 16:59:16 +0630

    [MIG] auth_block_signup: Migration to 15.0

---
+++ auth_block_signup/models/res_users.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    def action_reset_password(self):
+        if "http" in self.name:
+            raise UserError(_("Invalid name!"))
+        super(ResUsers, self).action_reset_password()
