54456f9397d65cb3f49282cc64e571262337d747
gamhpang <gamhpang@1812@gmail.com>
2022-04-13 10:20:57 +0630

    [MIG] auth_block_signup: Migration to 15.0

--- auth_block_signup/__manifest__.py
+++ auth_block_signup/__manifest__.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Block Signup",
     "version": "15.0.1.0.0",
     "category": "Extra Tools",
-    "website": "https://github.com/qrtl/mi7-custom",
+    "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "LGPL-3",
     "installable": True,
