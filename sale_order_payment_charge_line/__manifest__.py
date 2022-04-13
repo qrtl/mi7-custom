c1597165553f4de1abba0ca0b771e95706ec12d1
gamhpang <gamhpang@1812@gmail.com>
2022-04-13 13:24:18 +0630

    [MIG] sale_order_payment_charge_line: Migration to 15.0

--- sale_order_payment_charge_line/__manifest__.py
+++ sale_order_payment_charge_line/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Sale Order Payment Charge Line",
-    "version": "10.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Sale",
     "license": "LGPL-3",
     "author": "Quartile Limited",
