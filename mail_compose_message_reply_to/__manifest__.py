6860af3ead5f9b3135938b5d4004df999033dd93
gamhpang <gamhpang@1812@gmail.com>
2022-04-13 12:19:37 +0630

    [IMP] mail_compose_message: black, isort, prettier

--- mail_compose_message_reply_to/__manifest__.py
+++ mail_compose_message_reply_to/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
@@ -6,7 +5,7 @@
     "category": "Discuss",
     "license": "AGPL-3",
     "author": "Quartile Limited",
-    "website": "https://www.quartile.co",
+    "website": "https://github.com/qrtl/mi7-custom",
     "version": "10.0.1.0.0",
     "depends": ["mail"],
     "installable": True,
