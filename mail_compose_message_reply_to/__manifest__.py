fbd0430ada3e8f9e2f191ee484c4e2cf65620d99
gamhpang <gamhpang@1812@gmail.com>
2022-04-13 12:20:51 +0630

    [MIG] mail_compose_message_reply_to:Migration to 15.0

--- mail_compose_message_reply_to/__manifest__.py
+++ mail_compose_message_reply_to/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2021 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
@@ -5,8 +6,8 @@
     "category": "Discuss",
     "license": "AGPL-3",
     "author": "Quartile Limited",
-    "website": "https://github.com/qrtl/mi7-custom",
-    "version": "10.0.1.0.0",
+    "website": "https://www.quartile.co",
+    "version": "15.0.1.0.0",
     "depends": ["mail"],
     "installable": True,
 }
