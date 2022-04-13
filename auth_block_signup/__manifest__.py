44c985ea033e780a753cfaa0996a98d063acb30b
gamhpang <gamhpang@1812@gmail.com>
2022-04-13 13:08:51 +0630

    [IMP] auth_block_singup: black, isort, prettier" --no-verify

--- auth_block_signup/__manifest__.py
+++ auth_block_signup/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Block Signup",
     "version": "10.0.1.0.0",
     "category": "Extra Tools",
-    "website": "https://www.quartile.co/",
+    "website": "https://github.com/qrtl/mi7-custom",
     "author": "Quartile Limited",
     "license": "LGPL-3",
     "installable": True,
--- auth_block_signup/models/res_users.py
+++ auth_block_signup/models/res_users.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import _, api, models
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
