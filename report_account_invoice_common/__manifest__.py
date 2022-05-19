1fc76c1482d4064f90741c98ea03bdd97513204f
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-05-19 16:15:29 +0900

    [2426] miscellaneous updates (#192)
    Added:
    
    - account_view_adj
    - report_account_invoice_common
    - report_account_invoice_purchase
    - report_account_invoice_sale
    - report_sale_order
    - sale_order_subject
    
    Updated:
    
    - base_timezone_converter (no upgrade needed)
    - pro_mi7_license
    - pro_mi7_return
    - pro_mi7_sale
    - pro_mi7_website_sale_ec
    - product_intercompany_link
    - product_maker
    - purchase_create_sale_order
    - sale_user_type
    - website_portal_product
    - website_sale_delivery_date (no upgrade needed)
    
    Deleted:
    
    - pro_mi7_account
    - pro_mi7_report

---
+++ report_account_invoice_common/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2021 MI Seven Japan
+# Copyright 2022 Quartile Limited
+{
+    "name": "Report Account Invoice Common",
+    "website": "https://www.quartile.co.jp/",
+    "version": "10.0.1.0.0",
+    "author": "MI Seven Japan, Quartile Limited",
+    "depends": ["account"],
+    "installable": True,
+}
---
+++ report_account_invoice_common/models/account_invoice.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2021 MI Seven Japan
+# Copyright 2022 Quartile Limited
+
+from odoo import api, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.multi
+    def _get_factor(self):
+        self.ensure_one()
+        if self.type in ("out_invoice", "in_invoice"):
+            return 1
+        return -1
