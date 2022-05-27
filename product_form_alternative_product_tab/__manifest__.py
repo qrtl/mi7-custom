a3def9007f706766282e0dd8535f145d59d69178
Yoshi Tashiro <tashiro@quartile.co>
2022-05-27 09:43:03 +0000

    [MIG] product_form_alternative_product_tab: Migration to 15.0

--- product_form_alternative_product_tab/__manifest__.py
+++ product_form_alternative_product_tab/__manifest__.py
@@ -1,11 +1,13 @@
 # Copyright 2018-2021 MI Seven Japan
 # Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "Product Form Alternative Product Tab",
     "category": "Product",
-    "version": "10.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "MI Seven Japan, Quartile Limited",
-    "license": "Other proprietary",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
     "depends": ["website_sale"],
     "data": ["views/product_template_views.xml"],
     "installable": True,
