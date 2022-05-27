a7270b52acb71a24dd234501170de1ccb4d8979c
Yoshi Tashiro <tashiro@quartile.co>
2022-05-27 14:31:08 +0000

    [MIG] product_maker: Migration to 15.0

--- product_maker/__manifest__.py
+++ product_maker/__manifest__.py
@@ -4,10 +4,11 @@
 {
     "name": "Product Maker",
     "category": "Product",
-    "version": "10.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "MI Seven Japan, Quartile Limited",
+    "website": "https://www.quartile.co",
     "license": "LGPL-3",
-    "depends": ["sales_team"],
+    "depends": ["sale"],
     "data": [
         "security/ir.model.access.csv",
         "views/product_maker_views.xml",
