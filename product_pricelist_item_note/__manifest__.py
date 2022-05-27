d6517649f6a0fbcb5b16c6a2d4e33182c8911f2f
Yoshi Tashiro <tashiro@quartile.co>
2022-05-27 14:57:25 +0000

    [MIG] product_pricelist_item_note: Migration to 15.0

--- product_pricelist_item_note/__manifest__.py
+++ product_pricelist_item_note/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale Pricelist Line Note",
     "category": "Product",
-    "version": "10.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "license": "LGPL-3",
