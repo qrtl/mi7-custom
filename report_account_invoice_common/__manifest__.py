70ae88f2d996ddd87820c6e30666785793bf09a7
Yoshi Tashiro <tashiro@quartile.co>
2022-05-31 14:13:49 +0000

    [IMP] report_account_invoice_common: black, isort, prettier

--- report_account_invoice_common/__manifest__.py
+++ report_account_invoice_common/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018-2021 MI Seven Japan
 # Copyright 2022 Quartile Limited
 {
     "name": "Report Account Invoice Common",
-    "website": "https://www.quartile.co.jp/",
+    "website": "https://www.quartile.co",
     "version": "10.0.1.0.0",
     "author": "MI Seven Japan, Quartile Limited",
     "depends": ["account"],
