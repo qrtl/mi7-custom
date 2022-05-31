857c845ec6f4514d1e99b3512d602c51b1e281e7
Yoshi Tashiro <tashiro@quartile.co>
2022-05-31 15:25:05 +0000

    [IMP] company_chop: black, isort, prettier

--- company_chop/models/res_company.py
+++ company_chop/models/res_company.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018-2021 MI Seven Japan
 # Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
+
 
 class ResCompany(models.Model):
     _inherit = "res.company"
