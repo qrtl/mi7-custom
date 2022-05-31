7d3ebb604ae563b28a43251b6d7f493486ecda4e
Yoshi Tashiro <tashiro@quartile.co>
2022-05-31 07:34:30 +0000

    [IMP] sale_order_line_delivery_date: black, isort, prettier

--- sale_order_line_delivery_date/models/sale_order.py
+++ sale_order_line_delivery_date/models/sale_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018-2021 MI Seven Japan
 # Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
@@ -8,7 +7,7 @@ from odoo import fields, models
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
-    
+
     delivery_date = fields.Date(
         "Delivery Date",
         track_visibility="onchange",
