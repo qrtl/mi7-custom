7d2d84ce776ac10df7d1d497751fe9ee5fc7e54f
Yoshi Tashiro <tashiro@quartile.co>
2022-07-30 09:59:49 +0000

    [IMP] website_sale_cart_check: black, isort, prettier

--- website_sale_cart_check/controllers/main.py
+++ website_sale_cart_check/controllers/main.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import _, http
-from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo import http
 from odoo.http import request
+
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class WebsiteSale(WebsiteSale):
-
     def cart_content_validate(self, order):
         return []
-    
+
     @http.route()
     def cart(self, **post):
         res = super(WebsiteSale, self).cart(**post)
