8161ecce3b99c95dfd2a7e31c45e7c59ad9aac29
Yoshi Tashiro <tashiro@quartile.co>
2022-08-03 10:13:36 +0000

    [2655][FIX] payment_cod: message field visibility

--- payment_cod/models/payment_acquirer.py
+++ payment_cod/models/payment_acquirer.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PaymentAcquirer(models.Model):
@@ -14,3 +14,15 @@ class PaymentAcquirer(models.Model):
     )
     currency_id = fields.Many2one(related="company_id.currency_id")
     fee_product_id = fields.Many2one("product.product", string="Fee Product")
+
+    @api.depends("provider", "is_cod")
+    def _compute_view_configuration_fields(self):
+        super()._compute_view_configuration_fields()
+        self.filtered(lambda acq: acq.provider == "transfer" and acq.is_cod).write(
+            {
+                "show_pre_msg": True,
+                "show_pending_msg": False,
+                "show_done_msg": True,
+            }
+        )
+        return
