71cd69e89624923a49e7f4fd88dfa92c15b8b850
Yoshi Tashiro <tashiro@quartile.co>
2022-08-08 14:43:12 +0000

    [2675][ADD] product_related_product

---
+++ product_related_product/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Product Related Product",
+    "category": "Product",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "depends": ["product"],
+    "data": ["views/product_template_views.xml"],
+    "installable": True,
+}
