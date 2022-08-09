52666c4c75949b9444c809291f6036439631f1d6
Yoshi Tashiro <tashiro@quartile.co>
2022-08-09 07:16:25 +0000

    [MIG] portal_hide_shipping: Migration to 15.0 (renamed from website_sale_stock_hide_shipping)

--- portal_hide_shipping/__manifest__.py
+++ portal_hide_shipping/__manifest__.py
@@ -2,13 +2,13 @@
 # Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
-    "name": "Website Sale Stock Hide Shipping",
-    "category": "Website",
-    "version": "10.0.1.0.0",
+    "name": "Portal Hide Shipping",
+    "category": "Hidden",
+    "version": "15.0.1.0.0",
     "author": "MI Seven Japan, Quartile Limited",
     "website": "https://www.quartile.co",
     "license": "LGPL-3",
-    "depends": ["website_sale_stock"],
+    "depends": ["sale_stock"],
     "data": [
         "views/templates.xml",
     ],
