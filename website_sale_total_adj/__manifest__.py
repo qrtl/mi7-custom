0f6ee94561f6a30a2544587ca1eb33b39ac7e18b
Yoshi Tashiro <tashiro@quartile.co>
2022-08-09 08:42:10 +0000

    [MIG] website_sale_total_adj: Migration to 15.0

--- website_sale_total_adj/__manifest__.py
+++ website_sale_total_adj/__manifest__.py
@@ -4,8 +4,9 @@
 {
     "name": "Website Sale Total Adj",
     "category": "Website",
-    "version": "10.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "MI Seven Japan, Quartile Limited",
+    "website": "https://www.quartile.co",
     "license": "LGPL-3",
     "depends": ["website_sale"],
     "data": [
