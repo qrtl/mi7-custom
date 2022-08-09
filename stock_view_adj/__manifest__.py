312cdc27baa2d508cb07eb3390f156243750d30c
kanda999 <kanda@quartile.co>
2022-08-09 08:13:37 +0000

    [IMP] stock_view_adj: black, isort, prettier

--- stock_view_adj/__manifest__.py
+++ stock_view_adj/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
@@ -7,7 +6,7 @@
     "license": "LGPL-3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
-    "version": "10.0.1.0.0",
+    "version": "15.0.1.0.0",
     "depends": ["sale_stock"],
     "data": ["views/stock_picking_views.xml"],
     "installable": True,
