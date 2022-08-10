70534bc2b6ec0d64819532a96971c7946c5baf66
kanda999 <101688144+kanda999@users.noreply.github.com>
2022-08-10 16:51:39 +0900

    15st1 [2673][MIG] stock_view_adj (#162)
    [2673](https://www.quartile.co/web#id=2673&action=771&model=project.task&view_type=form&menu_id=505)

---
+++ stock_view_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021-2022 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock View Adjust",
+    "category": "Stock",
+    "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "15.0.1.0.0",
+    "depends": ["sale_stock"],
+    "data": ["views/stock_picking_views.xml"],
+    "installable": True,
+}
