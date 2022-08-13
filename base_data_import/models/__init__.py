2ba04977a2ddf647227ff4f093d7c66e04b5a587
Yoshi Tashiro <tashiro@quartile.co>
2022-08-13 12:22:33 +0000

    [IMP] base_data_import: black, isort, prettier

--- base_data_import/models/__init__.py
+++ base_data_import/models/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import data_import_error
 from . import data_import_log
--- base_data_import/wizards/data_import.py
+++ base_data_import/wizards/data_import.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020-2022 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
