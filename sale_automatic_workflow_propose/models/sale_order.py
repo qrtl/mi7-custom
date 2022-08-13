0b27b5f8d39f3bca119d17d3e9ada32cbf3e40c4
Yoshi Tashiro <tashiro@quartile.co>
2022-08-13 10:12:58 +0000

    [MIG] sale_automatic_workflow: Migration to 15.0

--- sale_automatic_workflow_propose/models/sale_order.py
+++ sale_automatic_workflow_propose/models/sale_order.py
@@ -1,5 +1,5 @@
 # Copyright 2021-2022 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
 
@@ -7,21 +7,26 @@ from odoo import api, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _update_vals(self, vals):
-        if vals.get("user_type"):
-            process = self.env["sale.workflow.process"].search(
-                [("apply_to", "=", vals["user_type"])]
-            )[:1]
-            if process:
-                vals["workflow_process_id"] = process.id
-        return vals
+    @api.model
+    def _get_workflow_process(self, user_type):
+        return self.env["sale.workflow.process"].search([("apply_to", "=", user_type)])[
+            :1
+        ]
 
     @api.model
     def create(self, vals):
-        vals = self._update_vals(vals)
-        return super(SaleOrder, self).create(vals)
+        order = super().create(vals)
+        if not order.user_type:
+            return order
+        process = self._get_workflow_process(order.user_type)
+        if process:
+            order.workflow_process_id = process.id
+        return order
 
-    @api.multi
     def write(self, vals):
-        vals = self._update_vals(vals)
-        return super(SaleOrder, self).write(vals)
+        if not vals.get("user_type"):
+            return super().write(vals)
+        process = self._get_workflow_process(vals.get("user_type"))
+        if process:
+            vals["workflow_process_id"] = process.id
+        return super().write(vals)
--- sale_automatic_workflow_propose/models/sale_workflow_process.py
+++ sale_automatic_workflow_propose/models/sale_workflow_process.py
@@ -1,5 +1,5 @@
 # Copyright 2021 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
@@ -10,6 +10,6 @@ class SaleWorkflowProcess(models.Model):
     # Keep the selectons consistent with user_type of sale.order
     apply_to = fields.Selection(
         [("b2c", "B2C"), ("b2b", "B2B")],
-        string="Apply To",
-        help="The workflow process will be proposed to the sales orders of the selected order type here.",
+        help="The workflow process will be proposed to the sales orders of the "
+        "selected order type here.",
     )
