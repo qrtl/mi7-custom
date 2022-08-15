9b1f9ddfdf5a7c1e7b6f1beb451f449aa484716c
Yoshi Tashiro <tashiro@quartile.co>
2022-08-15 16:51:08 +0800

    [2135][FIX] sale_automatic_workflow_propose
    Before this fix, workflow_process_id was not being proposed when sales order was created
    with user logged in, because the logic was relying on the assumption that 'user_type' would
    be available in vals, which wasn't the case due to the sequence of the create() extensions.

--- sale_automatic_workflow_propose/__manifest__.py
+++ sale_automatic_workflow_propose/__manifest__.py
@@ -7,7 +7,7 @@
     "license": "AGPL-3",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
-    "version": "10.0.1.1.0",
+    "version": "10.0.1.1.1",
     "depends": ["sale_automatic_workflow", "sale_user_type"],
     "data": ["views/sale_workflow_process_views.xml"],
     "installable": True,
