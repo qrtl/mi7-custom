3181ff57bc7b9c87c57224e4bf862faae73bb5ee
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-08-17 14:47:36 +0900

    [2135][FIX] sale_automatic_workflow_propose (#168)
    Before this fix, workflow_process_id was not being proposed when sales order was created
    with user logged in, because the logic was relying on the assumption that 'user_type' would
    be available in vals, which wasn't the case due to the sequence of the create() extensions.

--- sale_automatic_workflow_propose/models/sale_order.py
+++ sale_automatic_workflow_propose/models/sale_order.py
@@ -8,21 +8,27 @@ from odoo import api, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def _update_vals(self, vals):
-        if vals.get("user_type"):
-            process = self.env["sale.workflow.process"].search(
-                [("apply_to", "=", vals["user_type"])]
-            )[:1]
-            if process:
-                vals["workflow_process_id"] = process.id
-        return vals
+    @api.model
+    def _get_workflow_process(self, user_type):
+        return self.env["sale.workflow.process"].search([("apply_to", "=", user_type)])[
+            :1
+        ]
 
     @api.model
     def create(self, vals):
-        vals = self._update_vals(vals)
-        return super(SaleOrder, self).create(vals)
+        order = super(SaleOrder, self).create(vals)
+        if not order.user_type:
+            return order
+        process = self._get_workflow_process(order.user_type)
+        if process:
+            order.workflow_process_id = process.id
+        return order
 
     @api.multi
     def write(self, vals):
-        vals = self._update_vals(vals)
+        if not vals.get("user_type"):
+            return super(SaleOrder, self).write(vals)
+        process = self._get_workflow_process(vals.get("user_type"))
+        if process:
+            vals["workflow_process_id"] = process.id
         return super(SaleOrder, self).write(vals)
