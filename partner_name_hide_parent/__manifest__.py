5f7ee1719bc43b42ac2986c51b8711e4eda3c172
kanda999 <kanda@quartile.co>
2022-09-13 05:54:35 +0000

    [MIG] partner_name_hide_parent: Migration to 15.0

--- partner_name_hide_parent/__manifest__.py
+++ partner_name_hide_parent/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2020 Quartile Limited
+# Copyright 2020-2022 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Partner Name Hide Parent",
-    "version": "12.0.1.0.2",
+    "version": "15.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "category": "Extra Tools",
