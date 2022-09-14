1e8a99d4bcb94eea95a767427c306eb631f9581d
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-09-14 17:04:28 +0900

    [2318][ADD] mail_hide_send_message (#181)

---
+++ mail_hide_send_message/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Hide Send Message",
+    "category": "Discuss",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "15.0.1.0.0",
+    "depends": ["mail"],
+    "assets": {
+        "web.assets_qweb": [
+            "mail_hide_send_message/static/src/components/chatter_topbar/chatter_topbar.xml",
+        ],
+    },
+    "installable": True,
+}
