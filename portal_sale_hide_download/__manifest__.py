7cdde241769fd6b3cfc9d8d22117ae33b8d7bb2e
kanda999 <kanda@quartile.co>
2022-09-20 08:56:23 +0000

    [ADD] portal_sale_hide_download

---
+++ portal_sale_hide_download/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Portal Sale Hide Download",
+    "category": "Website",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "website": "https://www.quartile.co",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_portal_templates.xml",
+    ],
+    "installable": True,
+}
---
+++ portal_sale_hide_download/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Hide 'Download' 'Print' button in qualificaion and sale_order of portal page.
