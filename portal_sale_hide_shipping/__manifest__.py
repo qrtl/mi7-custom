28fccce4f92bef22a84a7e5d802fd2ba00e41394
Yoshi Tashiro <tashiro@quartile.co>
2022-09-21 06:11:13 +0000

    [MOV] portal_hide_shipping -> portal_sale_hide_shipping

--- portal_sale_hide_shipping/__manifest__.py
+++ portal_sale_hide_shipping/__manifest__.py
@@ -2,7 +2,7 @@
 # Copyright 2022 Quartile Limited
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
-    "name": "Portal Hide Shipping",
+    "name": "Portal Sale Hide Shipping",
     "category": "Hidden",
     "version": "15.0.1.0.0",
     "author": "MI Seven Japan, Quartile Limited",
