6f56ebd84e19c14c33f47f26e1971a041b8be126
Yoshi Tashiro <tashiro@quartile.co>
2022-10-06 15:51:07 +0000

    [2773][ADD] website_sale_confirmation_hide_print

---
+++ website_sale_confirmation_hide_print/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Portal Sale Confirmation Hide Print",
+    "category": "Website",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "website": "https://www.quartile.co",
+    "depends": ["website_sale"],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+}
