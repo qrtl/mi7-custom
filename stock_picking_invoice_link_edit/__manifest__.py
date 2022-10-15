51f7fc8e4a82f30916de7d64f2aff887d07c6b93
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-10-15 23:22:23 +0900

    [2854][ADD] stock_picking_invoice_link_edit (#207)

---
+++ stock_picking_invoice_link_edit/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Picking Invoice Link Edit",
+    "version": "15.0.1.0.0",
+    "category": "Warehouse Management",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["stock_picking_invoice_link"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
