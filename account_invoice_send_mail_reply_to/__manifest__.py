0415fd60ee7b49270d3d65aec688bd28031bf655
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-10-31 16:53:11 +0630

    [ADD] account_invoice_send_mail_reply_to

---
+++ account_invoice_send_mail_reply_to/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Invoice Send Reply To",
+    "category": "account",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "15.0.1.0.0",
+    "depends": ["account"],
+    "data": [
+        "views/account_invoice_send_view.xml",
+    ],
+    "installable": True,
+}
