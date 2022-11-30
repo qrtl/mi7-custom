5c26c6b2c1dbbb38e1f39c2ff92dcf01055f89f7
AungKoKoLin <45355704+AungKoKoLin1997@users.noreply.github.com>
2022-11-30 12:33:56 +0630

    [2883][ADD] website_sale_address_email_readonly #225
    [ADD] website_sale_address_email_readonly

---
+++ website_sale_address_email_readonly/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Address Email Readonly",
+    "category": "Ecommerce",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "15.0.1.0.0",
+    "depends": ["website_sale"],
+    "data": ["views/website_sale_address_view.xml"],
+    "installable": True,
+}
---
+++ website_sale_address_email_readonly/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Makes the address form email field in eCommerce checkout read-only if the user has email address.
