9a22aa28e4d88019707063ba0c3f62f0f82dbbfd
Rinaldi Firdaus <rinaldi@quartile.co>
2023-01-20 02:56:10 +0000

    [2999][ADD] website_product_description_view

---
+++ website_product_description_view/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Product Description View",
+    "version": "15.0.1.0.0",
+    "category": "website",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "depends": ["base", "website"],
+    "license": "AGPL-3",
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
---
+++ website_product_description_view/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module does the following:
+
+- Add new tab "Product Description for Website Display" on Product Master
+- Add fields Website Description field into the tab
+- Website Description field be able to be edited with HTML
