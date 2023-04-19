945f65804224d6ed21c54e363a4529e701bdbd7a
kanda999 <kanda@quartile.co>
2023-04-19 09:16:02 +0000

    [FIX] account_tax_round_down

--- account_tax_round_down/models/account_tax.py
+++ account_tax_round_down/models/account_tax.py
@@ -38,6 +38,9 @@ class AccountTax(models.Model):
             # This matters when currency is NOT given
             self = self.with_context(rounding_method=rounding_method)
             if currency:
+                # price_unit is calculated first with rouding. Because price_unit
+                # is not rounding down.
+                price_unit = currency.round(price_unit)
                 # If currency is given, the context is not passed down via self,
                 # and it should be assigned here.
                 currency = currency.with_context(rounding_method=rounding_method)
