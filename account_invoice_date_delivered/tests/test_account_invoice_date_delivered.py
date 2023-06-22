e54d665ca9c0ed984a43779ea03d2537758789d1
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-06-22 08:59:46 +0630

    [ADD] account_invoice_date_delivered: add tests #230
    [ADD] account_invoice_date_delivered: add test

---
+++ account_invoice_date_delivered/tests/test_account_invoice_date_delivered.py
@@ -0,0 +1,54 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestAccountInvoiceDateDelivered(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.product = cls.env["product.product"].create(
+            {"name": "Product1", "type": "product"}
+        )
+        cls.partner_id = cls.env["res.partner"].create(
+            {
+                "name": "Partner",
+            }
+        )
+        cls.so = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner_id.id,
+                "partner_invoice_id": cls.partner_id.id,
+                "partner_shipping_id": cls.partner_id.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product.name,
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 2,
+                            "product_uom": cls.product.uom_id.id,
+                            "price_unit": 300,
+                        },
+                    ),
+                ],
+            }
+        )
+
+    def update_product_qty(self, product):
+        product_qty = self.env["stock.change.product.qty"].create(
+            {
+                "product_id": product.id,
+                "product_tmpl_id": product.product_tmpl_id.id,
+                "new_quantity": 100.0,
+            }
+        )
+        product_qty.change_product_qty()
+
+    def test_01_account_invoice_date_delivered(self):
+        self.update_product_qty(self.product)
+        self.so.action_confirm()
+        pick = self.so.picking_ids
+        pick.move_line_ids.write({"qty_done": 2})
+        pick._action_done()
+        inv = self.so._create_invoices()
+        self.assertEqual(pick.date_done, inv.time_delivered)
