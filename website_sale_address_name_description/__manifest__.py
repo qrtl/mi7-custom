02bb1972e4eac30418dbbebbe892b816473ffef6
kanda999 <kanda@quartile.co>
2023-07-05 09:28:39 +0000

    Add address_name_text

--- website_sale_address_name_description/__manifest__.py
+++ website_sale_address_name_description/__manifest__.py
@@ -8,6 +8,6 @@
     "website": "https://www.quartile.co",
     "version": "15.0.1.0.0",
     "depends": ["website_sale"],
-    "data": ["views/website_sale_address_view.xml"],
+    "data": ["views/website_sale_address_view.xml", "views/res_company_views.xml"],
     "installable": True,
 }
---
+++ website_sale_address_name_description/models/res_company.py
@@ -0,0 +1,9 @@
+# Copyright 2023 Quartile Limited
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    address_name_text = fields.Html(translate=True, sanitize=False)
