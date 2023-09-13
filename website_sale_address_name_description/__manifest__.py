8e083715fb885c23cd8e50e75b6d5c0481ff73c3
Tatsuki Kanda (Quratile) <101688144+kanda999@users.noreply.github.com>
2023-09-13 11:06:59 +0900

    [3223][ADD] website_sale_address_name_description (#247)
    * [ADD] website_sale_address_name_description

---
+++ website_sale_address_name_description/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Website Sale Address Name Description",
+    "category": "Ecommerce",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "15.0.1.0.0",
+    "depends": ["website_sale"],
+    "data": ["views/website_sale_address_view.xml", "views/res_company_views.xml"],
+    "installable": True,
+}
---
+++ website_sale_address_name_description/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module does the following:
+
+- Add the description to address form name field in eCommerce checkout.
