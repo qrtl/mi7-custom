2f4ca89f5a96e2f439f2a11d6ffc42c410a1abcb
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2024-04-23 12:02:32 +0700

    [ADD] portal_user_login_email_change (#253)

---
+++ portal_user_login_email_change/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Portal User Login Email Change",
+    "category": "Portal",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "installable": True,
+}
