1ffd7aeb493e1093af15f1389081beb04ac4f40e
Aungkokolin1997 <lin@quartile.co>
2026-05-07 08:55:39 +0000

    fixup

--- product_tag/__manifest__.py
+++ product_tag/__manifest__.py
@@ -1,4 +1,4 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Part of Odoo.
 {
     "name": "Product Tag",
     "category": "Product",
--- product_tag/models/product_template.py
+++ product_tag/models/product_template.py
@@ -1,4 +1,4 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Part of Odoo.
 
 from odoo import fields, models
 
